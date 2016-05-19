61a98f9329afc0c5746b43250b157d06fd31ac0e
Jeremy Fix <jeremy.fix@gmail.com>
2016-05-19 16:27:16 +0200

    adds the use of the c++ library

--- cv2_grayscott.py
+++ cv2_grayscott.py
@@ -1,36 +1,50 @@
 import cv2
 import sys
 import random
+import numpy as np
 
 import grayscott
-
+import libgrayscott
 
 cv2.namedWindow('u')
-cv2.namedWindow('v')
+#cv2.namedWindow('v')
 
 key = 0
 run = False
 
 mode = int(sys.argv[1])
-N = 128
-model = grayscott.Model('solitons', N=N, mode=mode, measure_fps=True)
+N = 256
+pattern = 'worms'
+
+if(mode <= 2):
+    model = grayscott.Model(pattern, N=N, mode=mode)
+else:
+    model = libgrayscott.GrayScott(pattern, N)
+
 model.init()
 
 
+u = np.zeros((N, N))
+
 while key != ord('q'):
-	if(run): 
-		model.step()	
-	cv2.imshow('u', model.ut)
-	cv2.imshow('v', model.vt)
+    if(run): 
+        if(mode <= 2):
+            model.step()	
+            u[:,:] = model.ut
+        else:
+            u = model.step()
 
-	key = cv2.waitKey(1) & 0xFF
+    cv2.imshow('u', u)
+    #cv2.imshow('v', model.vt)
 
-	if(key == ord('c')):
-		c = (random.randint(0, N-1), random.randint(0, N-1))
-		cv2.circle(model.vt_1, c , N/4, 0, -1)
-	elif key == ord('s'):
-		run = not run
-		print("Running ? : " + str(run))
-	elif key == ord('i'):
-		model.init()
+    key = cv2.waitKey(1) & 0xFF
+
+    if(key == ord('c')):
+        c = (random.randint(0, N-1), random.randint(0, N-1))
+        cv2.circle(model.vt_1, c , N/4, 0, -1)
+    elif key == ord('s'):
+        run = not run
+        print("Running ? : " + str(run))
+    elif key == ord('i'):
+        model.init()
 
