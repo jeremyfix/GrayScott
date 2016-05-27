e47493ce0a24fb68c4e8791002a611d8c3eaf9c1
Jeremy Fix <jeremy.fix@gmail.com>
2016-05-27 09:11:02 +0200

    Makes use of the 2D cuda functions ; and therefore introduce the width/height term rather than just N as before

--- laplacian.cu
+++ laplacian.cu
@@ -1,15 +1,17 @@
 // nvcc -o laplacian laplacian.cu -std=c++11
+// nvcc -o laplacian laplacian.cu -std=c++11 -O3 -g -D_FORCE_INLINES
 #include <stdio.h>
 #include <cstdlib>
 #include <chrono>
 #include <cmath>
+#include <cstring>
 
 __global__
-void gpu_laplacian(unsigned int N, float* values, float* laplacian) {
+void gpu_laplacian(unsigned int width, unsigned int height, float* values, float* laplacian) {
 }
 
 __host__
-void cpu_laplacian(unsigned int N, float* values, float* laplacian) {
+void cpu_laplacian(unsigned int width, unsigned int height, float* values, float* laplacian) {
 
   // For efficiently computing the laplacian we will move 5 pointers on values
   // pointing : on the pixel, on its north, on its east, on its west, on its south
@@ -22,10 +24,10 @@ void cpu_laplacian(unsigned int N, float* values, float* laplacian) {
   // oooooooo
   // oooooooo
   // #oooooo#
-  laplacian[0] = -(4 * values[0]) + (values[(N-1)*N] + values[1] + values[N] + values[(N-1)]);// top left
-  laplacian[N-1] = -(4 * values[N-1]) + (values[(N-1)*N + (N-1)] + values[0] + values[N + (N-1)] + values[N-2]); // top right
-  laplacian[(N-1)*N] = -(4 * values[(N-1)*N]) + (values[(N-2)*N] + values[(N-1)*N + 1] + values[0] + values[(N-1)*N + (N-1)]); // bottom left
-  laplacian[(N-1)*N + (N-1)] = -(4 * values[(N-1)*N + (N-1)]) + (values[(N-2)*N + (N-1)]+values[(N-1)*N] + values[N-1] + values[(N-1)*N + (N-2)]); // bottom right
+  laplacian[0] = -(4 * values[0]) + (values[(height-1)*width] + values[1] + values[width] + values[width-1]);// top left
+  laplacian[width-1] = -(4 * values[width-1]) + (values[(height-1)*width + (width-1)] + values[0] + values[width + (width-1)] + values[width-2]); // top right
+  laplacian[(height-1)*width] = -(4 * values[(height-1)*width]) + (values[(height-2)*width] + values[(height-1)*width + 1] + values[0] + values[(height-1)*width + (width-1)]); // bottom left
+  laplacian[(height-1)*width + (width-1)] = -(4 * values[(height-1)*width + (width-1)]) + (values[(height-2)*width + (width-1)]+values[(height-1)*width] + values[width-1] + values[(height-1)*width + (width-2)]); // bottom right
 
   // We handle the borders
   // o########o
@@ -34,12 +36,12 @@ void cpu_laplacian(unsigned int N, float* values, float* laplacian) {
   // oooooooooo
   // oooooooooo
   v_ptr = values + 1;
-  vN_ptr = values + (N-1)*N + 1;
+  vN_ptr = values + (height-1)*width + 1;
   vE_ptr = values + 2;
-  vS_ptr = values + N + 1;
+  vS_ptr = values + width + 1;
   vW_ptr = values;
   l_ptr = laplacian + 1;
-  for(unsigned int i = 1 ; i < N-1 ; ++i, ++v_ptr, ++vN_ptr, ++vE_ptr, ++vS_ptr, ++vW_ptr, ++l_ptr) 
+  for(unsigned int i = 1 ; i < width-1 ; ++i, ++v_ptr, ++vN_ptr, ++vE_ptr, ++vS_ptr, ++vW_ptr, ++l_ptr) 
     *l_ptr = (*vN_ptr + *vE_ptr + *vS_ptr + *vW_ptr) - ((*v_ptr) * 4);
 
   // oooooooooo
@@ -47,13 +49,13 @@ void cpu_laplacian(unsigned int N, float* values, float* laplacian) {
   // oooooooooo
   // oooooooooo
   // o########o
-  v_ptr = values + (N-1)*N + 1;
-  vN_ptr = values + (N-2)*N + 1;
-  vE_ptr = values + (N-1)*N + 2;
+  v_ptr = values + (height-1)*width + 1;
+  vN_ptr = values + (height-2)*width + 1;
+  vE_ptr = values + (height-1)*width + 2;
   vS_ptr = values + 1 ;
-  vW_ptr = values + (N-1)*N;
-  l_ptr = laplacian + (N-1)*N + 1;
-  for(unsigned int i = 1 ; i < N-1 ; ++i, ++v_ptr, ++vN_ptr, ++vE_ptr, ++vS_ptr, ++vW_ptr, ++l_ptr) 
+  vW_ptr = values + (height-1)*width;
+  l_ptr = laplacian + (height-1)*width + 1;
+  for(unsigned int i = 1 ; i < width-1 ; ++i, ++v_ptr, ++vN_ptr, ++vE_ptr, ++vS_ptr, ++vW_ptr, ++l_ptr) 
     *l_ptr = (*vN_ptr + *vE_ptr + *vS_ptr + *vW_ptr) - ((*v_ptr) * 4);
 
   // oooooooooo
@@ -62,13 +64,13 @@ void cpu_laplacian(unsigned int N, float* values, float* laplacian) {
   // #ooooooooo
   // #ooooooooo
   // oooooooooo
-  v_ptr = values + N;
+  v_ptr = values + width;
   vN_ptr = values ;
-  vE_ptr = values + N + 1;
-  vS_ptr = values + 2*N ;
-  vW_ptr = values + N + (N-1);
-  l_ptr = laplacian + N;
-  for(unsigned int i = 1 ; i < N-1 ; ++i, v_ptr+=N, vN_ptr+=N, vE_ptr+=N, vS_ptr+=N, vW_ptr+=N, l_ptr+=N) 
+  vE_ptr = values + width + 1;
+  vS_ptr = values + 2*width ;
+  vW_ptr = values + width + (width-1);
+  l_ptr = laplacian + width;
+  for(unsigned int i = 1 ; i < height-1 ; ++i, v_ptr+=width, vN_ptr+=width, vE_ptr+=width, vS_ptr+=width, vW_ptr+=width, l_ptr+=width) 
     *l_ptr = (*vN_ptr + *vE_ptr + *vS_ptr + *vW_ptr) - ((*v_ptr) * 4);
 
  
@@ -79,13 +81,13 @@ void cpu_laplacian(unsigned int N, float* values, float* laplacian) {
   // ooooooooo#
   // oooooooooo
   // 
-  v_ptr = values + N + (N-1);
-  vN_ptr = values + (N-1);
-  vE_ptr = values + N;
-  vS_ptr = values + 2*N + (N-1) ;
-  vW_ptr = values + N + (N-2);
-  l_ptr = laplacian + N + (N-1);
-  for(unsigned int i = 1 ; i < N-1 ; ++i, v_ptr+=N, vN_ptr+=N, vE_ptr+=N, vS_ptr+=N, vW_ptr+=N, l_ptr+=N) 
+  v_ptr = values + width + (width-1);
+  vN_ptr = values + (width-1);
+  vE_ptr = values + width;
+  vS_ptr = values + 2*width + (width-1) ;
+  vW_ptr = values + width + (width-2);
+  l_ptr = laplacian + width + (width-1);
+  for(unsigned int i = 1 ; i < height-1 ; ++i, v_ptr+=width, vN_ptr+=width, vE_ptr+=width, vS_ptr+=width, vW_ptr+=width, l_ptr+=width) 
     *l_ptr = (*vN_ptr + *vE_ptr + *vS_ptr + *vW_ptr) - ((*v_ptr) * 4);
 
   // We handle the region inside the array exlucding a border of size 1,
@@ -95,15 +97,15 @@ void cpu_laplacian(unsigned int N, float* values, float* laplacian) {
   // o########o
   // o########o
   // oooooooooo
-  v_ptr = values + (1*N + 1);
+  v_ptr = values + (1*width + 1);
   vN_ptr = values + 1;
-  vE_ptr = values + (1*N + 2);
-  vS_ptr = values + (2*N + 1);
-  vW_ptr = values + (1*N + 0);
+  vE_ptr = values + (1*width + 2);
+  vS_ptr = values + (2*width + 1);
+  vW_ptr = values + (1*width + 0);
 
-  l_ptr = laplacian + (1*N + 1);
-  for(unsigned int i = 1; i < N-1; ++i) {
-    for(unsigned int j = 1 ; j < N-1 ; ++j, ++vN_ptr, ++vE_ptr, ++vS_ptr, ++vW_ptr, ++v_ptr, ++l_ptr) 
+  l_ptr = laplacian + (1*width + 1);
+  for(unsigned int i = 1; i < height-1; ++i) {
+    for(unsigned int j = 1 ; j < width-1 ; ++j, ++vN_ptr, ++vE_ptr, ++vS_ptr, ++vW_ptr, ++v_ptr, ++l_ptr) 
       *l_ptr = (*vN_ptr + *vE_ptr + *vS_ptr + *vW_ptr) - ((*v_ptr) * 4);
 
     // For switching to the next line we must move the pointers forward by 2 pixels
@@ -131,20 +133,19 @@ float diffNorm(float* v1, float* v2, unsigned int N) {
 
 __host__
 int main(int argc, char * argv[]) {
-  unsigned int N = 256;
-  unsigned int nbcalls = 100;
+
+  unsigned int width = 256;
+  unsigned int height = 256;
+  unsigned int nbcalls = 500;
 
   float *I, *lcpu, *lgpu, *dI, *dlgpu;
-  I = (float*) malloc(N*N*sizeof(float));
-  lcpu = (float*) malloc(N*N*sizeof(float));
-  lgpu = (float*) malloc(N*N*sizeof(float));
-
-  cudaMalloc(&dI, N*N*sizeof(float));
-  cudaMalloc(&dlgpu, N*N*sizeof(float));
+  I = (float*) malloc(width*height*sizeof(float));
+  lcpu = (float*) malloc(width*height*sizeof(float));
+  lgpu = (float*) malloc(width*height*sizeof(float));
 
   // Initialize the input image
   float *Iptr = I;
-  for(unsigned int i = 0 ; i < N*N; ++i, ++Iptr)
+  for(unsigned int i = 0 ; i < width*height; ++i, ++Iptr)
     (*Iptr) = std::rand() / ((float)RAND_MAX);
 
   //************* CPU *****************//
@@ -152,39 +153,47 @@ int main(int argc, char * argv[]) {
   start_cpu = std::chrono::system_clock::now();
 
   for(unsigned int i = 0 ; i < nbcalls; ++i) 
-    cpu_laplacian(N, I, lcpu);
+    cpu_laplacian(width, height, I, lcpu);
   
   end_cpu = std::chrono::system_clock::now();
   int elapsed_cpu_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end_cpu-start_cpu).count();
   printf("CPU elapsed : %f ms per call \n", ((float)elapsed_cpu_ms)/nbcalls);
-  //************* GPU *****************//
 
+  //************* GPU *****************//
+  
+  size_t pitch_dI, pitch_dlgpu;
+  cudaMallocPitch(&dI, &pitch_dI, width*sizeof(float), height);
+  cudaMallocPitch(&dlgpu, &pitch_dlgpu, width*sizeof(float), height);
+  
   std::chrono::time_point<std::chrono::system_clock> start_gpu, end_gpu;
   start_gpu = std::chrono::system_clock::now();
   
   // Copy the input to the GPU
-  cudaMemcpy(dI, I, N*N*sizeof(float), cudaMemcpyHostToDevice);
+  //cudaMemcpy(dI, I, N*N*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy2D(dI, pitch_dI, I, width*sizeof(float), width*sizeof(float), height, cudaMemcpyHostToDevice);
   
   // Call the kernel
   int threadsPerBlock = 256;
   int blocksPerGrid = 1;
-  gpu_laplacian<<<blocksPerGrid, threadsPerBlock>>>(N, dI, dlgpu);
+  gpu_laplacian<<<blocksPerGrid, threadsPerBlock>>>(width, height, dI, dlgpu);
   
   // Get the result
-  cudaMemcpy(lgpu, dlgpu, N*N*sizeof(float), cudaMemcpyDeviceToHost);
+  //cudaMemcpy(lgpu, dlgpu, N*N*sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy2D(lgpu, width*sizeof(float), dlgpu, width*sizeof(float), width*sizeof(float), height, cudaMemcpyDeviceToHost);
   
   end_gpu = std::chrono::system_clock::now();
   int elapsed_gpu_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end_gpu-start_gpu).count();
   printf("GPU elapsed : %f ms per call \n", ((float)elapsed_gpu_ms)/nbcalls);
-
+  
   //********** Comparison *************//
-  printf("Difference : %f \n", diffNorm(lcpu, lgpu, N));
+  printf("Difference : %f \n", diffNorm(lcpu, lgpu, width*height));
   
   //***********************************//
-  // Free the memory
+  // Free the device memory
   cudaFree(dI);
   cudaFree(dlgpu);
 
+  // Free the host memory
   delete[] I;
   delete[] lcpu;
   delete[] lgpu;
