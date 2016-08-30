2054d319f4d3f154c41d105ef2e732abf812d9f1
Jeremy Fix <jeremy.fix@gmail.com>
2016-08-30 17:49:49 +0200

    upd

--- cv2_grayscott.py
+++ cv2_grayscott.py
@@ -42,7 +42,7 @@ mode = int(sys.argv[1])
 #
 d = 1.0 # The width of the domain
 N = 100 # The size of the lattice
-dt = 0.01 # the time step
+dt = 1 # the time step
 pattern = 'worms'
 
 if(mode <= 2):
