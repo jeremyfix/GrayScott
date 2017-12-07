0a2726021e1455938b580879e2ed60e2b608747c
Jeremy Fix <jeremy.fix@gmail.com>
2017-12-07 17:22:36 +0100

    upd

---
+++ cv2_fitzhughNagumo.py
@@ -0,0 +1,150 @@
+# coding: utf-8
+# Simulation of the gray scott reaction diffusion system.
+#      /  
+#      |  ∂ₜu(x,t) =  ∇²u(x,t) + u(x,t) -u³(x,t) - v(x,t)
+#      |  ∂ₜv(x,t) =  δ ∇²v(x,t) + ε (u(x,t) - a₁ v(x,t) - a₀)
+#      \
+
+
+import cv2
+import sys
+import random
+import numpy as np
+import time
+import scipy
+
+import fitzhugh
+
+if(len(sys.argv) <= 1):
+    print("Usage : %s mode "% sys.argv[0])
+    print("With mode : ")
+    print("   0 : spatial model with ndimage.convolve in python, forward euler") # 165 fps
+    print("   1 : spectral model in python using ETDRK4")
+    sys.exit(-1)
+
+print(" Press : ")
+print("   s : start/pause")
+print("   i : reinitialize the concentrations")
+print("   q : quit")
+print("   c : erase the reactant v in a randomly chosen box patch")
+print("   m : mask the reactant with a randomly generated mask")
+print("   p : save the current u potential")
+print("   f : toggle fullscreen/normal screen")
+    
+
+try:
+    fullscreen_flag = cv2.WINDOW_FULLSCREEN
+    normal_flag = cv2.WINDOW_NORMAL
+except:
+    fullscreen_flag = cv2.cv.CV_WINDOW_FULLSCREEN
+    normal_flag = cv2.cv.CV_WINDOW_NORMAL
+
+cv2.namedWindow('u', cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("u", cv2.WND_PROP_FULLSCREEN, normal_flag)
+
+key = 0
+run = False
+
+mode = int(sys.argv[1])
+#
+if(mode == 0):
+    d = 1.5 # The width of the domain
+    height = 128 # The size of the lattice
+    width = 128
+    dt = 0.06 # the time step
+else:
+    d = 3.
+    height = 128
+    width = 256
+    dt = 10
+display_scaling_factor = 4
+pattern = 'labyrinth'
+
+if(mode == 0):
+    model = fitzhugh.Model(pattern, width=width, height=height, d=d, dt=dt)
+else:
+    model = fitzhugh.SpectralModel(pattern, width=width, height=height, d=d, dt=dt, mode='ETDFD')
+
+model.init()
+
+# Precompute the FFT of the kernel for speeding up the convolution
+# For lightning
+s_kernel = 11
+kernel_light = np.ones((s_kernel,s_kernel), dtype=np.float)
+kernel_light[:int(2./3 * s_kernel),:int(2./3 * s_kernel)] = -1
+mask_light = np.zeros((height, width), np.float32)
+mask_light[0:kernel_light.shape[0], 0:kernel_light.shape[1]] = kernel_light
+mask_light = np.roll(np.roll(mask_light,-(kernel_light.shape[1]//2-1), axis=1),-(kernel_light.shape[0]//2-1), axis=0)
+fft_mask_light = np.fft.rfft2(mask_light)
+# For the blur, it is fastest to use uniform_filter
+
+
+def make_effect(u_orig, scale):
+    res_height, res_width = scale * u_orig.shape[0], scale * u_orig.shape[1]
+
+    # Compute the lightning effect
+    effect = np.fft.irfft2(np.fft.rfft2(2.*(u_orig-0.5))* fft_mask_light)
+    
+    effect /= 30. # HAND TUNED SCALING of the effect ... might need to be adapted if changing s_kernel
+    effect[effect >= 1.0] = 1.0
+    effect[effect <= 0.0] = 0.0
+    effect_hires = cv2.resize(effect, (res_width, res_height), interpolation=cv2.INTER_CUBIC)
+
+    u_hires = cv2.resize(u_orig, (res_width, res_height),interpolation=cv2.INTER_CUBIC)
+    u_hires[u_hires >= 0.5] = 1.
+    u_hires[u_hires < 0.5 ] = 0.
+    
+    # Blur the image to get the shading
+    u_blur = scipy.ndimage.filters.uniform_filter(u_hires, size=5)
+    
+    # Shift the shadding down right
+    u_blur = np.lib.pad(u_blur, ((2,0),(2,0)), 'constant', constant_values=1)[:-2,:-2]
+    
+    dst = 0.6 * u_hires + 0.4 * effect_hires
+    dst[u_hires >= 0.99] = u_blur[u_hires >= 0.99]
+    dst[dst > 1] = 1
+    dst[dst < 0] = 0
+    return dst
+
+
+u = np.zeros((height, width))
+epoch = 0
+
+t0 = time.time()
+frame_id = 0
+while key != ord('q'):
+    if(run): 
+        model.step()
+        u[:,:] = model.get_ut()
+	epoch += 1
+	if(epoch % 100 == 0):
+	    t1 = time.time()
+	    print("FPS: %f fps  / t = %f" % (100 / (t1 - t0), epoch * dt))
+	    t0 = t1
+        
+        #u_img = make_effect(u, display_scaling_factor)
+        cv2.imshow('u', u)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if(key == ord('c')):
+        c = (random.randint(0, N-1), random.randint(0, N-1))
+        model.erase_reactant(c , N/8)
+    elif(key == ord('m')):
+        mask = 0.75 + 0.25*np.random.random((N, N))
+        model.mask_reactant(mask)
+    elif key == ord('s'):
+        run = not run
+        print("Running ? : " + str(run))
+    elif key == ord('i'):
+        model.init()
+    elif key == ord('p'):
+        print("Saving u-%05d.png" % frame_id)
+        cv2.imwrite("u-%05d.png" % frame_id, (np.minimum(255*u_img, 255)).astype(np.uint8))
+        frame_id += 1
+    elif key == ord('f'):
+        screenmode = cv2.getWindowProperty("u", cv2.WND_PROP_FULLSCREEN)
+        if(screenmode == normal_flag):
+            cv2.setWindowProperty("u", cv2.WND_PROP_FULLSCREEN, fullscreen_flag)
+        else:
+            cv2.setWindowProperty("u", cv2.WND_PROP_FULLSCREEN, normal_flag)
---
+++ fitzhugh.py
@@ -0,0 +1,299 @@
+# coding: utf-8
+
+import numpy as np
+import scipy.signal
+import scipy.ndimage
+import numpy as np
+import time
+import sys
+
+
+''' 
+We consider a spatial domain of size d × d, with N × N samples
+
+Expressed in the spectral domain, the system we solve is 
+
+OUTDATED !!!!!
+
+ ∂ₜ U [k₁,k₂] = -[Dᵤ ( (2πk₁/d)^2 + (2πk₂/d)^2)] U[k₁,k₂] - TF[TF^-1(U) (TF^-1(V))^2] + F N^2 δ_{k₁,k₂} - F U[k₁,k₂] 
+ ∂ₜ V [k₁,k₂] = -[Dᵥ ( (2πk₁/d)^2 + (2πk₂/d)^2)] V[k₁,k₂] + TF[TF^-1(U) (TF^-1(V))^2] - (F + k) V[k₁,k₂]
+
+with U = TF(u) , V = TF(v)
+
+If we decompose the linear and non-linear parts of the equations, following the notations of "Fourth order time-stepping for stiff PDEs, the system reads:
+
+ ∂ₜ U[k₁, k₂] = Lᵤ U[k₁,k₂] + Nᵤ(U[k₁,k₂], V[k₁,k₂]) + F N^2 δ_{k₁,k₂}
+ ∂ₜ V[k₁, k₂] = Lᵥ V[k₁,k₂] - Nᵤ(U[k₁,k₂], V[k₁,k₂])
+
+with Lᵤ U[k₁,k₂]            = -[Dᵤ ( (2πk₁/d)^2 + (2πk₂/d)^2) + F] U[k₁,k₂]
+     Nᵤ(U[k₁,k₂], V[k₁,k₂]) = -TF[TF^-1(U) (TF^-1(V))^2]
+     Lᵥ V[k₁,k₂]            = -[Dᵥ ( (2πk₁/d)^2 + (2πk₂/d)^2) + (F + k)] V[k₁,k₂]
+and we should not forget the term F δ_{k₁,k₂} which introduces a F dt δ_{k₁,k₂} in the integration with dt the time step
+
+we can then use the formulas of Cox and Mathews with the numerical stabilization procedure of Kassam, Trefethen for computing
+the terms like (e^z - 1)/z with the Cauchy Integral
+
+References:
+ - Notes on FFT-based differentiation, [Johnson, 2011]
+ - Fourth-order time stepping for stiff PDEs,  [Kassam, Trefethen, 2005]
+'''
+# class SpectralModel:
+#     ''' Mode can be in ETDFD or ETDRK4 '''
+#     def __init__(self, param_name, width, height, d=1., dt=0.1, mode='ETDFD'):
+#         self.param_name = param_name
+#         if(self.param_name == 'solitons'):
+#             self.k = 0.056
+#             self.F = 0.020
+#         elif(self.param_name == 'worms'):
+#             self.k = 0.0630
+#             self.F = 0.0580
+#         elif(self.param_name == 'spirals'):			
+#             self.k = 0.050
+#             self.F = 0.018
+#         elif(self.param_name == 'exp'):
+#             self.k = 0.0594
+#             self.F = 0.0460
+#         else:
+#             self.k = 0.040
+#             self.F = 0.060
+#         self.width = width
+#         self.height = height
+#         self.h = d/self.width
+#         self.d = d
+#         self.Du = 2 * 1e-5 / self.h**2
+#         self.Dv = 1e-5 / self.h**2
+#         self.dt = dt
+#         self.noise = 0.2
+
+#         #self.tf_ut_1 = np.zeros((self.N, self.N), dtype=complex)
+#         #self.tf_vt_1 = np.zeros((self.N, self.N), dtype=complex)
+#         self.cdtype = np.complex64
+#         self.fdtype = np.float32
+#         self.tf_ut = np.zeros((self.height, self.width), dtype=self.cdtype)
+#         self.tf_vt = np.zeros((self.height, self.width), dtype=self.cdtype)
+
+#         self.mode = mode
+#         if(not self.mode in ['ETDFD','ETDRK4']):
+#             print("The numerical scheme you mentioned is not implemented")
+#             raise Exception("Unknown numerical scheme, must be ETDFD or ETDRK4")
+
+#         # Precompute various ETDRK4 scalar quantities
+#         k1, k2 = np.meshgrid(np.arange(self.width).astype(float), np.arange(self.height).astype(self.fdtype))
+#         k1[:,self.width/2+1:] -= self.width
+#         k2[self.height/2+1:,:] -= self.height
+
+#         k1 *= 2.0 * np.pi / self.width
+#         k2 *= 2.0 * np.pi / self.height
+
+#         self.Lu = -(self.Du * (k1**2 + k2**2) + self.F)
+#         print(self.Lu.dtype)
+#         self.Lv = -(self.Dv * (k1**2 + k2**2) + self.F + self.k)
+
+#         self.E2u = np.exp(self.dt * self.Lu/2.)
+#         self.Eu = self.E2u ** 2
+        
+#         self.E2v = np.exp(self.dt * self.Lv/2.)
+#         self.Ev = self.E2v ** 2
+
+#         M = 16 # Nb of points for complex means
+#         r = (np.exp(1j * np.pi * (np.arange(M)+0.5)/M)).reshape((1, M))
+#         # Generate the points along the unit circle contour over which to compute the mean
+#         LRu = (self.dt * self.Lu).reshape((self.width*self.height, 1)) + r
+#         LRv = (self.dt * self.Lv).reshape((self.width*self.height, 1)) + r
+        
+#         # The matrix for integrating the constant F term in the equation of u
+#         self.F2u = -np.real(np.mean(self.dt * (1. - np.exp(LRu/2.))/LRu, axis=1).reshape((self.height, self.width)))
+#         self.F2u[1:,:] = 0
+#         self.F2u[:,1:] = 0
+#         self.Fu = -np.real(np.mean(self.dt * (1. - np.exp(LRu))/LRu, axis=1).reshape((self.height, self.width)))
+#         self.Fu[1:,:] = 0
+#         self.Fu[:,1:] = 0
+#         if(mode == 'ETDFD'):
+#             self.FNu = -np.real(np.mean(self.dt * (1. - np.exp(LRu))/LRu, axis=1).reshape((self.height, self.width)))
+#             self.FNv = -np.real(np.mean(self.dt * (1. - np.exp(LRv))/LRv, axis=1).reshape((self.height, self.width)))
+#         elif(mode == 'ETDRK4'): 
+#             LRu_2 = LRu**2.
+#             LRu_3 = LRu**3.
+#             self.Qu = np.real(np.mean(self.dt * (np.exp(LRu/2.) - 1.) / LRu, axis=1).reshape((self.height, self.width)))
+#             self.f1u = np.real(np.mean(self.dt * (-4. - LRu + np.exp(LRu) * (4. - 3 * LRu + LRu_2)) / LRu_3, axis=1).reshape((self.height, self.width)))
+#             self.f2u = np.real(np.mean(self.dt * 2. * (2. + LRu + np.exp(LRu) * (-2. + LRu)) / LRu_3, axis=1).reshape((self.height, self.width)))
+#             self.f3u = np.real(np.mean(self.dt * (-4. - 3 * LRu - LRu_2 + np.exp(LRu) * (4. - LRu)) / LRu_3, axis=1).reshape((self.height, self.width)))
+
+#             LRv_2 = LRv**2.
+#             LRv_3 = LRv**3.
+#             self.Qv = np.real(np.mean(self.dt * (np.exp(LRv/2.) - 1.) / LRv, axis=1).reshape((self.height, self.width)))
+#             self.f1v = np.real(np.mean(self.dt * (-4. - LRv + np.exp(LRv) * (4. - 3 * LRv + LRv_2)) / LRv_3, axis=1).reshape((self.height, self.width)))
+#             self.f2v = np.real(np.mean(self.dt * 2. * (2. + LRv + np.exp(LRv) * (-2. + LRv)) / LRv_3, axis=1).reshape((self.height, self.width)))
+#             self.f3v = np.real(np.mean(self.dt * (-4. - 3 * LRv - LRv_2 + np.exp(LRv) * (4. - LRv)) / LRv_3, axis=1).reshape((self.height, self.width)))
+
+#     def init(self):
+#         dN = min(self.height, self.width)/4
+        
+#         ut = np.zeros((self.height, self.width), dtype=np.float32)
+#         ut[:,:] = 1
+#         ut[(self.height/2 - dN/2): (self.height/2+dN/2+1), (self.width/2 - dN/2) : (self.width/2+dN/2+1)] = 0.5
+#         ut += self.noise * (2 * np.random.random((self.height, self.width)) - 1)
+#         ut[ut <= 0] = 0
+
+#         vt = np.zeros((self.height, self.width), dtype=float)
+#         vt[:,:] = 0
+#         vt[(self.height/2 - dN/2): (self.height/2+dN/2+1), (self.width/2 - dN/2) : (self.width/2+dN/2+1)] = 0.25
+#         vt += self.noise * (2 * np.random.random((self.height, self.width)) - 1)
+#         vt[vt <= 0] = 0
+        
+#         self.tf_ut = np.fft.fft2(ut)
+#         self.tf_vt = np.fft.fft2(vt)
+       
+#     def get_ut(self):
+#         return np.real(np.fft.ifft2(self.tf_ut))
+    
+#     def get_vt(self):
+#         return np.real(np.fft.ifft2(self.tf_vt))
+
+    
+#     def compute_Nuv(self, tf_u, tf_v):
+#         uv2 = np.fft.fft2(np.fft.ifft2(tf_u).real * (np.fft.ifft2(tf_v).real**2))
+#         return -uv2, uv2
+
+#     # Erase the reactant in a box
+#     def erase_reactant(self, center, radius):
+#         vt =np.real(np.fft.ifft2(self.tf_vt))
+#         vt[(center[0]-radius):(center[0]+radius), (center[1]-radius):(center[1]+radius)] = 0
+#         self.tf_vt = np.fft.fft2(vt)      
+
+#     # Mask the reactant,
+#     # mask.shape = self.height, self.width
+#     # mask.dtype = float
+#     # mask_ij in [0, 1]
+#     def mask_reactant(self, mask):
+#         #vt =np.real(np.fft.ifft2(self.tf_vt))
+#         #vt = vt * mask
+#         #self.tf_vt = np.fft.fft2(vt)
+
+
+#         vt = np.real(np.fft.ifft2(self.tf_vt))
+#         vt[mask >= 0.5] = 1.0
+#         self.tf_vt = np.fft.fft2(vt)
+        
+#         ut = np.real(np.fft.ifft2(self.tf_ut))
+#         ut[mask >= 0.5] = 0.0
+#         self.tf_ut = np.fft.fft2(ut)
+
+    
+    
+#     def step(self):
+#         if(self.mode == 'ETDFD'):
+#             Nu, Nv = self.compute_Nuv(self.tf_ut, self.tf_vt)
+#             self.tf_ut = self.Eu * self.tf_ut + self.Fu * self.F * self.width * self.height + self.FNu * Nu 
+#             self.tf_vt = self.Ev * self.tf_vt + self.FNv * Nv 
+#         elif(self.mode == 'ETDRK4'):
+#             Nu, Nv = self.compute_Nuv(self.tf_ut, self.tf_vt)
+#             au = self.E2u * self.tf_ut + self.F2u * self.F *self.width*self.height+ self.Qu * Nu
+#             av = self.E2v * self.tf_vt + self.Qv * Nv
+#             Nau, Nav = self.compute_Nuv(au, av)
+#             bu = self.E2u * self.tf_ut + self.F2u * self.F * self.width * self.height + self.Qu * Nau
+#             bv = self.E2v * self.tf_vt + self.Qv * Nav
+#             Nbu, Nbv = self.compute_Nuv(bu, bv)
+#             cu = self.E2u * au + self.F2u * self.F * self.width * self.height + self.Qu * (2. * Nbu - Nu)
+#             cv = self.E2v * av + self.Qv * (2. * Nbv - Nv)
+#             Ncu, Ncv = self.compute_Nuv(cu, cv)
+
+#             self.tf_ut = self.Eu * self.tf_ut + self.Fu * self.F * self.width * self.height + self.f1u * Nu + self.f2u * (Nau + Nbu) + self.f3u * Ncu
+#             self.tf_vt = self.Ev * self.tf_vt + self.f1v * Nv + self.f2v * (Nav + Nbv) + self.f3v * Ncv
+
+        
+class Model:
+
+    def __init__(self, param_name, width, height,d=1.,dt=0.1):
+        self.param_name = param_name
+        if(self.param_name == 'labyrinth'):
+            self.a0 = -0.1
+            self.a1 = 2
+            self.epsilon = 0.05
+            self.delta = 4.
+        self.width = width
+        self.height = height
+        self.h = d/self.width
+        self.dt = dt
+        self.noise = 0.2
+        
+        self.ut_1 = np.zeros((self.height, self.width), dtype=float)
+        self.vt_1 = np.zeros((self.height, self.width), dtype=float)
+        self.ut = np.zeros((self.height, self.width), dtype=float)
+        self.vt = np.zeros((self.height, self.width), dtype=float)
+        
+        self.stencil = np.array([[0, 1., 0], [1., -4., 1.], [0, 1., 0]], dtype=float)
+
+    def init(self):
+        dN = self.width/32
+        self.ut_1[:,:] = 0
+        self.ut_1[:, (self.width/2-dN):(self.width/2+dN)] = 1
+        #self.ut_1 += self.noise * (2 * np.random.random((self.height, self.width)) - 1)
+        self.ut_1[self.ut_1 <= 0] = 0
+        for i in range(self.height):
+            shift = int(5*np.exp(-(i - self.height/2.)**2/(2.*10.**2))*np.cos(i*2.*np.pi/20) + (2.0 * np.random.random() - 1.)* 3.)
+            self.ut_1[i,:] = np.roll(self.ut_1[i,:], shift)
+
+        
+        self.vt_1[:,:] = 0        
+        
+        self.vt[:,:] = 0
+        self.ut[:,:] = self.ut_1[:,:]
+        
+    def laplacian(self, x):
+        return scipy.ndimage.convolve(x, self.stencil, mode='wrap')
+    
+    def get_ut(self):
+        return self.ut
+
+    def erase_reactant(self, center, radius):
+        pass
+
+    def step(self):
+        lu = self.laplacian(self.ut_1)
+        lv = self.laplacian(self.vt_1)	
+        self.ut[:,:] = self.ut_1 + self.dt * (lu + self.ut_1 - self.ut_1**3 - self.vt_1)
+        self.vt[:,:] = self.vt_1 + self.dt * (self.delta * lv + self.epsilon * (self.ut_1 - self.a1 * self.vt_1 - self.a0))
+        
+        self.ut_1, self.vt_1  = self.ut, self.vt
+
+            
+
+if(__name__ == '__main__'):
+
+    if(len(sys.argv) <= 1):
+        print("Usage : %s mode "% sys.argv[0])
+        print("With mode : ")
+        print("   0 : spatial model with ndimage.convolve in python, forward euler") # 165 fps
+        print("   1 : spectral model in python using ETDRK4")
+
+        sys.exit(-1)
+
+    mode = int(sys.argv[1])
+    
+    height = 256
+    width = 256
+    pattern = 'labyrinth'
+    d = 1.
+    dt = 0.01
+
+    if(mode == 0):
+        model = Model(pattern, width, height)
+    elif mode == 1:
+        model = None #SpectralModel(pattern, height=height, width=width)
+
+    model.init()
+    
+    epoch = 0
+    t0 = time.time()
+    
+    while True:
+        model.step()
+        epoch += 1
+        if(epoch % 500 == 0):
+            t1 = time.time()
+            print("FPS : %f f/s" % (500 / (t1 - t0)))
+            t0 = t1
+
+
+
